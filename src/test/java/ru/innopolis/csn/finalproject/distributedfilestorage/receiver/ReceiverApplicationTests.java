fadc0dd08c6d142718da0d2f064aca2093725fa8
Gleb Smetanin <g.o.smetanin@gmail.com>
2023-10-23 21:00:59 +0300

    Initial commit

---
+++ src/test/java/ru/innopolis/csn/finalproject/distributedfilestorage/receiver/ReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.innopolis.csn.finalproject.distributedfilestorage.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
